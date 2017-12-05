08ba2b7bc743855d495a6c615ef2bdd0585d2ebe
phi99 <34248917+phi99@users.noreply.github.com>
2017-12-06 09:54:23 +1100

    Update stock1.py

--- stock1.py
+++ stock1.py
@@ -18,8 +18,7 @@ os.chdir(path)
 filename_per = '../results/perAug2016.txt'
 filename_pbr = '../results/pbrAug2016.txt'
 filename_price = '../results/price27Aug2016.txt'
-					
-							
+											
 def cnbcKeyStats(saham):							
     try:							
         sourceCode = urllib2.urlopen('http://testxyz.com/quotes/%s/tab/7'%saham).read()							
@@ -29,26 +28,22 @@ def cnbcKeyStats(saham):
         with open(os.path.join(path,filename), 'wb') as fid:							
 	    fid.write(sourceCode)
 
-		#calculate stock parameter
+	#calculate stock parameter
         per = sourceCode.split('P/E Ratio (TTM)</td>')[1].split('<td class="data">')[1].split('</td>')[0]
-	    pbr = sourceCode.split('Price to Book (MRQ)</td>')[1].split('<td class="data">')[1].split('</td>')[0]
-		price = fid.read().split('<span style="font-size: 23px;">')[1].split('</span>')[0].strip()
+	pbr = sourceCode.split('Price to Book (MRQ)</td>')[1].split('<td class="data">')[1].split('</td>')[0]
+	price = fid.read().split('<span style="font-size: 23px;">')[1].split('</span>')[0].strip()
 		
-		#write parameter values to file
-	    with open(filename_per, 'a') as fod:
-        fod.write('per ' + a + " " + per + "\n")	
-		
-		with open(filename_pbr, 'a') as fod:
-        fod.write('pbr ' + a + " " + pbr + "\n")
-		
-		with open(filename_price, 'a') as fod:
-        fod.write('price ' + a + " " + price + "\n")
+	#write parameter values to file
+	with open(filename_per, 'a') as fod:
+            fod.write('per ' + a + " " + per + "\n")	
+	with open(filename_pbr, 'a') as fod:
+            fod.write('pbr ' + a + " " + pbr + "\n")
+	with open(filename_price, 'a') as fod:
+            fod.write('price ' + a + " " + price + "\n")
 		
     except Exception,e:							
         print 'failed in the main loop',str(e)							
 							
-		
-
 for eachStock in emiten_list:
     cnbcKeyStats(eachStock)
     time.sleep(1)
