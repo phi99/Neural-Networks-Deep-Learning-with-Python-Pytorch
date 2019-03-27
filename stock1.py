9a08213bc7d9e4ca8b8ccad853ccef1887d12c81
phi99 <34248917+phi99@users.noreply.github.com>
2019-03-28 03:47:38 +1100

    Update stock1.py

--- stock1.py
+++ stock1.py
@@ -11,7 +11,7 @@ timestr = time.strftime("%Y%m%d-%H%M%S")
 with open('emiten.txt') as f:							
     emiten_list=[x.strip() for x in f]							
 							
-path='/home/ubuntu12/saham2/Reuters2'							
+path='/home/ubuntu12/stock2/Reuters2'							
 os.makedirs(path)		
 os.chdir(path)
 
@@ -19,10 +19,10 @@ filename_per = '../results/perAug2016.txt'
 filename_pbr = '../results/pbrAug2016.txt'
 filename_price = '../results/price27Aug2016.txt'
 											
-def cnbcKeyStats(saham):							
+def cnbcKeyStats(stock):							
     try:							
-        sourceCode = urllib2.urlopen('http://testxyz.com/quotes/%s/tab/7'%saham).read()							
-        filename = saham + "_" + timestr + "_testxyz.html"
+        sourceCode = urllib2.urlopen('http://testxyz.com/quotes/%s/tab/7'%stock).read()							
+        filename = stock + "_" + timestr + "_testxyz.html"
 
         #save the html page to path							
         with open(os.path.join(path,filename), 'wb') as fid:							
@@ -46,4 +46,4 @@ def cnbcKeyStats(saham):
 							
 for eachStock in emiten_list:
     cnbcKeyStats(eachStock)
-    time.sleep(1)
+time.sleep(1)
