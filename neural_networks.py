f5a76f4ac53c06ffc0fcb9a26e08a3afa4ba247f
phi99 <34248917+phi99@users.noreply.github.com>
2025-08-31 08:16:59 +1000

    Update neural_networks.py

--- neural_networks.py
+++ neural_networks.py
@@ -4,15 +4,15 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class Model(nn.Module):
-  def __init__(self, in_features=4, h1=8, h2=9, out_features=3):
+  def __init__(self, in_features=4, h1=5, h2=7, out_features=2):
     super().__init__()
-    self.fc1=nn.Linear(in_features, h1)
-    self.fc2=nn.Linear(h1,h2)
+    self.layer1=nn.Linear(in_features, h1)
+    self.layer2=nn.Linear(h1,h2)
     self.out=nn.Linear(h2, out_features)
 
   def forward(self,x):
-    x=F.relu(self.fc1(x))
-    x=F.relu(self.fc2(x))
+    x=F.relu(self.layer1(x))
+    x=F.relu(self.layer2(x))
     x=self.out(x)
 
     return x
